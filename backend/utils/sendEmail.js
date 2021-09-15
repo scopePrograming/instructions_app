934d5f4fb5af1f989fe393ffcc7c72d8a034716f
adham-source <adham.ahmad1988as@gmail.com>
2021-09-15 18:27:20 +0200

    Reset pass

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+const sendEmail = async (email, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST_RES_PASSWORD,
+            service: process.env.SERVEICE_RES_PASS,
+            port: 587,
+            secure: true,
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            }
+        })
+
+        await transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject: subject,
+            text: text,
+        })
+
+        console.log("email sent sucessfully");
+    } catch (error) {
+        console.log(error, "email not sent");
+    }
+}
+
+module.exports = sendEmail
